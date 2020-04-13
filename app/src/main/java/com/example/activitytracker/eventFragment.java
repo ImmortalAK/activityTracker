b3f686ab122c79209c7d80df1b33803b791ef738
Alex Kirby <42648007+ImmortalAK@users.noreply.github.com>
2020-04-13 16:43:49 -0400

    Fragment and calendar interact with one another but the buttons do not work yet. (I have tried to code the first option but am experiencing crashes.)

--- app/src/main/java/com/example/activitytracker/eventFragment.java
+++ app/src/main/java/com/example/activitytracker/eventFragment.java
@@ -20,7 +20,7 @@ public class eventFragment extends Fragment {
 
 
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER 
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
