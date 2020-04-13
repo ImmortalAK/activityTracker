48dcea1f6f7f0f3c69c3b11356f8c7a3efe6dd48
Alex Kirby <42648007+ImmortalAK@users.noreply.github.com>
2020-04-13 16:32:48 -0400

    Fragment and calendar interact with one another but the buttons do not work yet. (I have tried to code the first option but am experiencing crashes.)

--- app/src/main/java/com/example/activitytracker/MainActivity.java
+++ app/src/main/java/com/example/activitytracker/MainActivity.java
@@ -1,14 +1,41 @@
 package com.example.activitytracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
+    private CalendarView mCalendarView;
+    private Object eventFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+                public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                setContentView(R.layout.fragment_event);
+                /*eventFragment newFragment = new eventFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+// Replace whatever is in the fragment_container view with this fragment,
+// and add the transaction to the back stack
+                transaction.add(R.id.optionsView, newFragment);
+// Commit the transaction
+                transaction.commit();*/
+                }
+        });
     }
 }
---
+++ app/src/main/java/com/example/activitytracker/eventFragment.java
@@ -0,0 +1,81 @@
+package com.example.activitytracker;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link eventFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class eventFragment extends Fragment {
+
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public eventFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment eventFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static eventFragment newInstance(String param1, String param2) {
+        eventFragment fragment = new eventFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_event, container, false);
+    }
+
+    public void toastMsg(String msg) {
+
+        Toast toast = Toast.makeText(getActivity().getApplicationContext(), msg, Toast.LENGTH_LONG);
+        toast.show();
+
+    }
+
+    public void displayToastMsg(View v) {
+
+        toastMsg("Hello how are you today!!");
+
+    }
+}
